080cd402cafeb17acdf27f0743b5f37a2086f559
howdy-mj <hi.minjungkim@gmail.com>
2020-05-07 17:54:56 +0900

    setting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./index.scss";
+import "./style/reset.scss";
+import "./style/common.scss";
 
 ReactDOM.render(
   <React.StrictMode>
